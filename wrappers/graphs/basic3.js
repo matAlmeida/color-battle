bdc94a10e8b624d7e737f28fae0891c26f51aced
Matheus Almeida <mat.almeida@live.com>
2018-12-04 10:12:33 -0300

    feat: new graph

---
+++ wrappers/graphs/basic3.js
@@ -0,0 +1,132 @@
+export default [
+  {
+    label: 1,
+    links: [21, 25],
+    pos: [1, 8]
+  },
+  {
+    label: 2,
+    links: [21, 25],
+    pos: [2, 5]
+  },
+  {
+    label: 3,
+    links: [21, 25],
+    pos: [4, 4]
+  },
+  {
+    label: 4,
+    links: [21, 25],
+    pos: [1, 7]
+  },
+  {
+    label: 5,
+    links: [21, 25],
+    pos: [3, 3]
+  },
+  {
+    label: 6,
+    links: [21, 25],
+    pos: [0, 8]
+  },
+  {
+    label: 7,
+    links: [21, 25],
+    pos: [3, 5]
+  },
+  {
+    label: 8,
+    links: [21, 25],
+    pos: [5, 3]
+  },
+  {
+    label: 9,
+    links: [21, 25],
+    pos: [6, 2]
+  },
+  {
+    label: 10,
+    links: [21, 25],
+    pos: [2, 9]
+  },
+  {
+    label: 11,
+    links: [21, 25],
+    pos: [3, 7]
+  },
+  {
+    label: 12,
+    links: [21, 25],
+    pos: [3, 3]
+  },
+  {
+    label: 13,
+    links: [21, 25],
+    pos: [7, 2]
+  },
+  {
+    label: 14,
+    links: [21, 25],
+    pos: [4, 8]
+  },
+  {
+    label: 15,
+    links: [21, 25],
+    pos: [9, 7]
+  },
+  {
+    label: 16,
+    links: [21, 25],
+    pos: [8, 5]
+  },
+  {
+    label: 17,
+    links: [21, 25],
+    pos: [4, 9]
+  },
+  {
+    label: 18,
+    links: [21, 25],
+    pos: [8, 8]
+  },
+  {
+    label: 19,
+    links: [21, 25],
+    pos: [9, 6]
+  },
+  {
+    label: 20,
+    links: [21, 25],
+    pos: [9, 3]
+  },
+  {
+    label: 21,
+    links: [25],
+    pos: [8, 2]
+  },
+  {
+    label: 22,
+    links: [25],
+    pos: [6, 9]
+  },
+  {
+    label: 23,
+    links: [25],
+    pos: [8, 7]
+  },
+  {
+    label: 24,
+    links: [25],
+    pos: [10, 4]
+  },
+  {
+    label: 25,
+    links: [],
+    pos: [9, 5]
+  },
+  {
+    label: 26,
+    links: [],
+    pos: [8, 4]
+  }
+];
