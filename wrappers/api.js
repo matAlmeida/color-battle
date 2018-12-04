3fd018faf974cd270ee501d07e2c976b36e33208
Matheus Almeida <mat.almeida@live.com>
2018-12-04 10:17:31 -0300

    feat: new graph wrapper

--- wrappers/api.js
+++ wrappers/api.js
@@ -1,53 +1,16 @@
-export const getGraph = id => {
-  const mainGraph = [
-    {
-      label: 1,
-      pos: [5, 0],
-      links: [2, 6, 7, 9]
-    },
-    {
-      label: 2,
-      pos: [9, 0],
-      links: [3, 9]
-    },
-    {
-      label: 3,
-      pos: [9, 4],
-      links: [4, 9]
-    },
-    {
-      label: 4,
-      pos: [6, 7],
-      links: [5, 8]
-    },
-    {
-      label: 5,
-      pos: [2, 5],
-      links: [6, 7, 8]
-    },
-    {
-      label: 6,
-      pos: [0, 1],
-      links: [7]
-    },
-    {
-      label: 7,
-      pos: [2, 3],
-      links: [8]
-    },
-    {
-      label: 8,
-      pos: [5, 5],
-      links: [9]
-    },
-    {
-      label: 9,
-      pos: [7, 2],
-      links: []
-    }
-  ];
+import { basic1, basic2, basic3 } from "./graphs";
 
-  return mainGraph;
+export const getGraph = (id = 1) => {
+  switch (id) {
+    case 1:
+      return basic1;
+    case 2:
+      return basic2;
+    case 3:
+      return basic3;
+    default:
+      return [];
+  }
 };
 
 export const getLinks = graph => {
--- wrappers/graphs/index.js
+++ wrappers/graphs/index.js
@@ -2,8 +2,4 @@ import basic1 from "./basic1";
 import basic2 from "./basic2";
 import basic3 from "./basic3";
 
-export default {
-  basic1,
-  basic2,
-  basic3
-};
+export { basic1, basic2, basic3 };
