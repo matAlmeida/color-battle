f84d8e13ae8b11c32e738bda2eb89657fefa2245
Matheus Almeida <mat.almeida@live.com>
2018-12-04 12:57:50 -0300

    Update basic2.js

--- wrappers/graphs/basic2.js
+++ wrappers/graphs/basic2.js
@@ -6,47 +6,47 @@ export default [
   },
   {
     label: 2,
-    links: [1, 3, 7],
+    links: [3, 7],
     pos: [1, 4]
   },
   {
     label: 3,
-    links: [1, 2, 4, 5],
+    links: [4, 5],
     pos: [1, 5]
   },
   {
     label: 4,
-    links: [1, 2, 5, 6],
+    links: [5, 6],
     pos: [1, 6]
   },
   {
     label: 5,
-    links: [2, 4, 6, 8, 9],
+    links: [6, 8, 9],
     pos: [2, 5]
   },
   {
     label: 6,
-    links: [4, 5, 9],
+    links: [9],
     pos: [2, 6]
   },
   {
     label: 7,
-    links: [2, 8, 10],
+    links: [8, 10],
     pos: [3, 4]
   },
   {
     label: 8,
-    links: [5, 7, 9, 10],
+    links: [9, 10],
     pos: [3, 5]
   },
   {
     label: 9,
-    links: [5, 6, 8, 10],
+    links: [10],
     pos: [3, 6]
   },
   {
     label: 10,
-    links: [7, 8, 9],
+    links: [],
     pos: [4, 5]
   }
 ];
