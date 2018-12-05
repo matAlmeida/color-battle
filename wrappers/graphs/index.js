02274aa1f96057b831a15fb8514ff87016621775
Gfgoes <34280600+Gfgoes@users.noreply.github.com>
2018-12-05 10:19:14 -0200

    Update index.js

--- wrappers/graphs/index.js
+++ wrappers/graphs/index.js
@@ -1,5 +1,6 @@
 import basic1 from "./basic1";
 import basic2 from "./basic2";
 import basic3 from "./basic3";
+import basic4 from "./basic4";
 
-export { basic1, basic2, basic3 };
+export { basic1, basic2, basic3, basic4 };
