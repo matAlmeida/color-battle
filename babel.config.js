d9e9399f7f5f553e836122c248338fe23918b24a
Matheus Almeida <mat.almeida@live.com>
2019-01-18 16:23:37 -0300

    refac from 0

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@app": "./src/app",
+            "@assets": "./src/assets",
+            "@common": "./src/app/common",
+            "@constants": "./src/constants",
+            "@services": "./src/services",
+            "@store": "./src/app/store"
+          }
+        }
+      ]
+    ]
   };
 };
