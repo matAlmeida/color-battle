4541fcce05094e999ddd520ba7fba804938ddaea
Matheus Almeida <mat.almeida@live.com>
2019-01-18 19:21:44 -0300

    linear gradient and button

--- src/app/signIn/SignInScreen.js
+++ src/app/signIn/SignInScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Text, Image, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
+import { LinearGradient } from "expo";
+
+import Layout from "@constants/Layout";
 import { Container } from "@common";
 
 export default class SignInScreen extends Component {
@@ -18,17 +21,24 @@ export default class SignInScreen extends Component {
   render() {
     return (
       <Container style={styles.container}>
-        <Image
-          source={require("@assets/images/logo-color-battle.png")}
-          resizeMode="center"
-          style={styles.image}
+        <LinearGradient
+          colors={["#00edff", "#00ffbb"]}
+          style={styles.gradientBackground}
+          start={[1, 0]}
+          end={[0, 1]}
         />
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleText}>color battle</Text>
+        </View>
         <View style={styles.contentConteiner}>
           <Text>Oi</Text>
         </View>
-        <View style={styles.newAccount}>
-          <Text>Criar conta com email -></Text>
-        </View>
+        <TouchableHighlight
+          style={styles.newAccountButton}
+          onPress={() => console.log("Criar nova conta")}
+        >
+          <Text style={styles.newAccountText}>Criar conta com email</Text>
+        </TouchableHighlight>
       </Container>
     );
   }
@@ -38,18 +48,32 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: "space-between"
   },
-  image: {
+  gradientBackground: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: Layout.window.height
+  },
+  titleContainer: {
     height: 150,
-    paddingVertical: 20
+    paddingVertical: 40
+  },
+  titleText: {
+    fontFamily: "coolvetica",
+    fontSize: 38
   },
   contentConteiner: {
     flex: 1
   },
-  newAccount: {
+  newAccountButton: {
     justifyContent: "center",
     alignItems: "center",
     height: 50,
     width: "100%",
-    backgroundColor: "rgba(0, 0, 0, .5)"
+    backgroundColor: "rgba(0, 0, 0, .3)"
+  },
+  newAccountText: {
+    color: "#FFF"
   }
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { ActivityIndicator, View } from "react-native";
+import { Font } from "expo";
 
 import Router from "./router";
 
-const App = () => <Router />;
+class App extends React.Component {
+  state = {
+    fontLoaded: false
+  };
+
+  componentDidMount = async () => {
+    await Font.loadAsync({
+      coolvetica: require("@assets/fonts/coolvetica-rg.ttf")
+    });
+
+    this.setState({ fontLoaded: true });
+  };
+
+  render() {
+    const { fontLoaded } = this.state;
+
+    if (fontLoaded) {
+      return <Router />;
+    }
+
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#00ffbb" />
+      </View>
+    );
+  }
+}
 
 export default App;
